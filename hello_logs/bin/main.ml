4595bb486757b6c9e3667da41a66e2365ab41e70
hbobenicio <hbobenicio@gmail.com>
2024-12-24 19:05:37 -0300

    hello_logs example

---
+++ hello_logs/bin/main.ml
@@ -0,0 +1,26 @@
+open Printf
+
+let timestamp_str () : string =
+    (** returns a string representing the current timestamp *)
+    let now: Unix.tm = Unix.gmtime (Unix.gettimeofday ()) in
+    sprintf "%04d-%02d-%02d %02d:%02d:%02d"
+        (1900 + now.tm_year)
+        (now.tm_mon + 1)
+        now.tm_mday
+        now.tm_hour now.tm_min now.tm_sec
+
+let timestamped_reporter () : Logs.reporter =
+    let pp_header (ppf: Format.formatter) ((level, header): Logs.level * string option) =
+        let header: string = Option.value header ~default:"" in
+        let timestamp = timestamp_str () in
+        Fmt.pf ppf "%s [%a] %s" timestamp Logs.pp_level level header
+    in
+    Logs_fmt.reporter ~pp_header ()
+
+let () =
+    Logs.set_level (Some Logs.Info);
+    Logs.set_reporter (timestamped_reporter ());
+    Logs.info (fun log -> log "Hello");
+    Logs.err (fun log -> log "Woops!");
+    if Logs.err_count () > 0 then
+        exit 1;
